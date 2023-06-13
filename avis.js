7b87ec03e24e44992132c2ac613c09f4a215510f
elena chibrit <elenachibrit93@gmail.com>
2023-06-13 17:10:53 +0200

    import f dans pieces.js + appel

--- avis.js
+++ avis.js
@@ -1,4 +1,13 @@
 //export (pour rendre le fonction accesible dans le autres fichiers)
 export function ajoutClientsAvis() {
+  //On recupere tous le boutons Avis pour leur ajouter un eventListener click
     const piecesElements = document.querySelectorAll(".fiches article button")
+    for(let i = 0; i >= piecesElements.length; i++){
+      piecesElements.addEventListener("click", function(event){
+
+        const id = event.target.dataset.id;
+        fetch(`http://localhost:8081/pieces/${id}/avis`);
+      });
+
+    }
 }
